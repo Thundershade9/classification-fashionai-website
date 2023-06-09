df118e5a2fae7d211442a8bd6a8d80ac53ae08f1
Thundershade9 no <dappythunder@gmail.com>
2023-06-09 13:45:47 +0000

    tryrte thrtdht

---
+++ classification.py
@@ -0,0 +1,21 @@
+# Import the necessary libraries 
+import tensorflow as tf
+from tensorflow import keras 
+import numpy as np 
+import matplotlib.pyplot as plt 
+# Load the Fashion MNIST dataset 
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=1000)
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+model.save('fashion_mnist_model.h5')
---
+++ routes.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template 
+from PIL import Image
+import numpy as np
+from tensorflow import keras
+
+app = Flask(__name__) # Load the model
+loaded_model = keras.models.load_model('fashion_mnist_model.h5') # Define the class names
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+@app.route('/') 
+def home():
+    return render_template('home.html')
+def predict_image(image):
+     image = image.reshape(1, 28, 28) # reshape the image to the input shape of the model
+     prediction = loaded_model.predict(image)
+     predicted_class = np.argmax(prediction)
+     return class_names[predicted_class]
+
+@app.route('/predict', methods=['POST'])
+ def predict(): file = request.files['file'] # get the file
+    image = Image.open(file).convert('L') # convert image to grayscale
+    image = image.resize((28, 28)) # resize image to 28x28
+    image = np.array(image) / 255.0 # normalize the pixel values
+    prediction = predict_image(image) return render_template('prediction.html', prediction=prediction)
+if __name__ == "__main__":
+    app.run(debug=True)
